48f007856c34acba28ed27d6719d4a7e696e41e3
SOHAG-SHU <sohag209366@gmail.com>
2024-03-06 18:18:30 +0600

    Add Variable

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,44 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!!");
+    private int roll;
+    private String name;
+    private boolean passed;
+    public HelloWorld(){ /* Constructor */
+        roll =500;
+        name = "SOHAG";
+        passed = false;
     }
-    
+    public HelloWorld(int roll, String name, boolean passed){ /* Constructor */
+        this.roll = roll;
+        this.name = name;
+        this.passed = passed;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        System.out.println(helloWorld.getName());
+        System.out.println(helloWorld.getRoll());
+        System.out.println(helloWorld.isPassed());
+    }
 }
